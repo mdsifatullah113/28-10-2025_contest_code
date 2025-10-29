d18aed9594d9910e2d5d26c1dcc963227cf61845
sifat <s2410476116@ru.ac.bd>
2025-10-29 07:51:05 +0600

    String-B

---
+++ cak.cpp
@@ -0,0 +1,32 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+	int t; cin>>t;
+	while(t--){
+		int a,b,c,d;
+		cin>>a>>b>>c>>d;
+		int mn=min(a,b),mx=max(a,b);
+		bool flag1=false;
+		bool flag2=false;
+		for(int i=mn;i<=mx;i++){
+			if(i==c ){
+				flag1 = true;
+			}
+			if(i==d){
+				flag2 = true;
+			}
+		}
+		
+		if( (!flag1 && flag2) ||(flag1 && !flag2)){
+			cout<<"YES\n";
+		}
+		else{
+			cout<<"NO\n";
+
+		}
+		
+	}
+
+	return 0;
+}
